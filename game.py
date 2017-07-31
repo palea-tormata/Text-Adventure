31f2523013879615859e132587005d00e3be7a60
palea-tormata <fritulanagrani@gmail.com>
2017-07-31 16:56:24 +0200

    separate files

--- game.py
+++ game.py
@@ -1,53 +1,27 @@
-location_names = ["Castle","East Bridge","West Bridge","Woods","Lake"]
-where_to = [[1,2],[0,3],[0,4],[1],[2]]
+from player import Player
 
-class Player:
-    def __init__(self, name, position, hp):
-        self.name = name
-        self.position = position
-        self.hp = hp
-    def my_name(self):
-        print("Your name is " + self.name)
-    def my_health(self):
-        print("You have " + self.hp + " hp")    
-    def where_am_I(self):
-        print("You are at the " + location_names[self.position])
-
-    def move(self):
-        print(where_can_I_go(self))
-        to = raw_input("Where to? > ")
-        if to not in location_names:
-            print("You can't go there..")
-        else:
-            self.position = location_names.index(to)
-        
-
-# return a list of places (string format) you can move to
-def where_can_I_go(player):
-    where = []
-    for pos in where_to[player.position]:
-        where.append(location_names[pos])
-    return where
-        
-
-
-
+# TODO 
+# input clean-up required (case sensitive)
+# unexpected input
+# list the possibilities for given situation
 
 if __name__ == "__main__":
     # player is prompted to input a name
     player_name = raw_input("Enter your name > ")
     # select difficulty -> different starting hp
     difficulty  = raw_input("Select difficulty (Easy, Medium, Hard) > ")
-    # TODO input clean-up required
     
     if difficulty == "Easy":
         starting_hp = 20
     elif difficulty == "Medium":
         starting_hp = 15
-    else:
+    elif difficulty == "Hard":
         starting_hp = 10
+    else:
+        print("That's not an option")
+        exit(1)
 
-    # starting position is at the Castle
+    # starting position is at the Castle (0)
     player = Player(player_name, 0, starting_hp)
 
     print("The game has started!")
@@ -59,7 +33,10 @@ if __name__ == "__main__":
         elif action == "move":
             player.move()
         elif action == "where":
-            print(where_can_I_go(player))
+            player.where_can_I_go()
+        else:
+            print("That's not an option")
+            exit(1)
 
     print("Thank you for playing")
     exit(0) 
